70b7b09c59c7d04dad6c569a89e385fee9ed16a4
NoyBenYehuda <126175893+NoyBenYehuda@users.noreply.github.com>
2023-02-23 17:06:21 +0200

    Update QUARY_GAIA_DATA.py

--- QUARY_GAIA_DATA.py
+++ QUARY_GAIA_DATA.py
@@ -35,20 +35,6 @@ q = Gaia.launch_job_async(query, output_format='votable')
 table = q.get_results()
 
 
-# Add a new columns for data based on the photometry data
-distance = 1.0  # Assume 1 kpc distance
-g=table['phot_g_mean_mag']
-abs_mag = table['phot_g_mean_mag'] - 5 * (np.log10(distance) - 1)
-luminosity = 10 ** (-0.4 * (abs_mag - 4.77))
-table['luminosity'] = luminosity
-tef=table['teff_gspphot']
-# Add a new column for effective temperature based on the color index
-table['teff'] = 4600 * (1 / (.92 * table['bp_rp'] + 1.7) + 1 / (.92 * table['bp_rp'] + 0.62))
-mh=table['mh_gspphot']
-#eExport the table
-table.write('gaia_data.fits', overwrite=True)
-rv=table['radial_velocity']
-
 end_time = time.time()
 total_time = end_time - start_time
 print(f"Time passed: {total_time:.2f} seconds")
